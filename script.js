60c205ab2f89c9f7324fbc45bd36bbdaccb212cd
Mario Enrique Cuberos Hernández <ing.mariocuberos@gmail.com>
2022-06-01 14:39:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@
   $form.addEventListener("submit", (e) => {
     e.preventDefault();
     $loader.classList.remove("none");
-    fetch("https://formsubmit.co/ajax/tefys.gz@gmail.com", {
+    fetch("https://formsubmit.co/ajax/ing.mariocuberos@gmail.com", {
       method: "POST",
       body: new FormData(e.target),
     })
